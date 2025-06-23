7848082c7c2aacaa9d0bd95cc1d402aaf02b6800
SkyD666 <akarinnn666@gmail.com>
2025-06-23 20:12:32 +0800

    [feature] Add PaddingValues.minus

--- shared/src/commonMain/kotlin/com/skyd/compone/ext/PaddingValuesExt.kt
+++ shared/src/commonMain/kotlin/com/skyd/compone/ext/PaddingValuesExt.kt
@@ -18,6 +18,16 @@ operator fun PaddingValues.plus(other: PaddingValues): PaddingValues = PaddingVa
 )
 
 @Composable
+operator fun PaddingValues.minus(other: PaddingValues): PaddingValues = PaddingValues(
+    top = calculateTopPadding() - other.calculateTopPadding(),
+    bottom = calculateBottomPadding() - other.calculateBottomPadding(),
+    start = calculateStartPadding(LocalLayoutDirection.current) -
+            other.calculateStartPadding(LocalLayoutDirection.current),
+    end = calculateEndPadding(LocalLayoutDirection.current) -
+            other.calculateEndPadding(LocalLayoutDirection.current)
+)
+
+@Composable
 operator fun PaddingValues.plus(other: Dp): PaddingValues = this + PaddingValues(other)
 
 @Composable
